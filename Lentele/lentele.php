fa3dc6781ee8b8a4a91190944a1a5ac510a965cc
dvasia-PC <ingridababarikovaite@gmail.com>
2018-05-09 15:52:05 +0300

    Bandymas

---
+++ Lentele/lentele.php
@@ -0,0 +1,40 @@
+<?php
+// Prisijungimo duomenys
+$servername = 'localhost';
+$dbname = 'Auto';
+$username = 'Auto';
+$password = 'LabaiSlaptas123';
+
+// Prisijungiame prie duomenų bazės 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+header('Content-Type: application/json');
+
+if ($conn->connect_error) {
+    echo json_encode([
+        success => false,
+        error => $conn->connect_error
+        ]);
+    exit;
+}
+
+// Suformuojame SELECT užklausą
+$sql = 'SELECT * FROM radars';
+
+// Vykdome suformuotą užklausą duomenų bazėje
+$result = $conn->query($sql); 
+
+$auto = [];
+
+// Tikriname ar rezultate yra bent viena eilutė
+if ($result->num_rows > 0) { 
+    while($row = $result->fetch_assoc()) {
+        $auto[] = $row;
+    }
+}
+
+echo json_encode([
+    'success' => true,
+    'data' => $auto
+]);
+
